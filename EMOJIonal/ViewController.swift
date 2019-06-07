f247f63fc02f013c9a9d80d4474c3f419753204e
Apple <apple@Apples-MacBook-Pro-95.local>
2019-06-07 16:25:49 -0700

    Second Iteration

--- EMOJIonal/ViewController.swift
+++ EMOJIonal/ViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+   
+   
+    @IBAction func showPartyMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "TIME TO PARTY!", message: "Honestly the best mood to be in, live it up!", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
     }
-
+    
+    
+    @IBAction func showHeartMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "WE GET IT YOU'RE IN LOVE", message: "Anyone would be lucky to have you😘", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func showQueenMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "QUEEN!", message: "werk👏🏻it👏🏻 don't let anyone dull your sparkle", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func showBamMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "A POWERHOUSE!", message: "be your best self", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func showSadMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "man it SUCKS to be sad", message: "it's normal! cry it out, eat some ice cream", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func showCowboyMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "yeehaw", message: "hawyee", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
 
 }
 
